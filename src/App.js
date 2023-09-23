f3e3536709ba7fe315a221c9ade8dbdc45a84c92
Yaduhh <putra61002@gmail.com>
2023-09-23 19:16:54 +0700

    ReactJS_PROJECT

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <p>Ini Overview</p>
+    </div>
+  );
+}
+
+export default App;
