b1dfbc5fd945a2240ad8d982a39a5dd7b035fc38
Mohammad Nizar Ardansyah <82304620+NizarArdansyah@users.noreply.github.com>
2023-11-10 14:34:19 +0700

    initial components

---
+++ src/screens/cover.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function CoverScreen() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <div className="w-[428px] h-screen flex flex-col items-center justify-evenly overflow-auto"></div>
+    </div>
+  );
+}
