883d4faf0f9a8d2d4472c5d3764be60aa431eaca
Yaduhh <putra61002@gmail.com>
2023-11-16 14:45:50 +0700

    Culture-C101

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import NavigationBottom from "./components/bottom-navigation";
 import DiskAudio from "./components/disk-audio";
 import classNames from "classnames";
 import CoverScreen from "./screens/cover";
+import { AiFillInstagram } from "react-icons/ai";
+import { TbMapSearch } from "react-icons/tb";
+import Countdown from "./components/coutdown";
+import { LuCopy } from "react-icons/lu";
 
 function App() {
   const [openCover, setOpenCover] = useState(false);
@@ -125,33 +129,240 @@ function App() {
             <div className="absolute w-full -z-20 top-0 scale-125">
               <img src="./assets/awan.png" alt="awan" />
             </div>
-            <div className="text-white">
-              <h1>THE WEDDING OF</h1>
+            <div className="text-white text-center space-y-64 mt-10">
+              <div>
+                <h1 className="text-Cardo">THE WEDDING OF</h1>
+                <h1 className="font-Parisienne text-4xl">Ahmad & Mutiara</h1>
+              </div>
+              <p>12 DESEMBER 2024</p>
             </div>
           </section>
-          <section
-            className="h-screen bg-[#F5F0F4] flex flex-col items-center justify-evenly"
-            id="pasangan"
-          >
-            Pasangan
+
+          <section id="pasangan">
+            <div className="h-screen bg-[#222222] flex flex-col relative gap-3 -z-0 overflow-hidden items-center justify-center">
+              <div className="absolute w-full bottom-0 right-0 -z-10 blur-sm">
+                <img src="./assets/hiasanpasangan.png" alt="hiasan" />
+              </div>
+              <div className="text-end text-white font-Cardo -mt-10 mb-2">
+                <p className="text-3xl">
+                  WE ARE GETTING <br />
+                  MARRIED
+                </p>
+              </div>
+              <p className="text-4xl text-white font-Parisienne">
+                Ahmad Almayda
+              </p>
+              <div className="text-white flex items-center gap-2 font-Cardo">
+                <AiFillInstagram size={25} />
+                <p>@donjoni_</p>
+              </div>
+              <div className="text-white text-center">
+                <p className="font-Courgette text-xl">
+                  Putra ke tiga dari keluarga
+                </p>
+                <p className="font-Cardo">Bpk. Kasmadi & Ibu Suheni</p>
+              </div>
+              <div className="w-[65%] mt-5">
+                <img src="./assets/pria.png" alt="pria" />
+              </div>
+            </div>
+            {/* perempuan */}
+            <div className="h-screen bg-[#222222] flex flex-col relative gap-3 -z-0 overflow-hidden items-center justify-center">
+              <div className="absolute w-full bottom-0 left-0 -z-10 -scale-x-100 blur-sm">
+                <img src="./assets/hiasanpasangan.png" alt="hiasan" />
+              </div>
+              <div className="text-end text-white font-Cardo -mt-10 mb-2">
+                <p className="text-3xl">
+                  WE ARE GETTING <br />
+                  MARRIED
+                </p>
+              </div>
+              <p className="text-4xl text-white font-Parisienne">
+                Mutiara Devania
+              </p>
+              <div className="text-white flex items-center gap-2 font-Cardo">
+                <AiFillInstagram size={25} />
+                <p>@araauhuy_</p>
+              </div>
+              <div className="text-white text-center">
+                <p className="font-Courgette text-xl">
+                  Putri ke tiga dari keluarga
+                </p>
+                <p className="font-Cardo">Bpk. Kasmadi & Ibu Suheni</p>
+              </div>
+              <div className="w-[65%] mt-5">
+                <img src="./assets/perempuan.png" alt="pria" />
+              </div>
+            </div>
           </section>
+
           <section
-            className="h-screen bg-[#F5F0F4] flex flex-col items-center justify-evenly"
+            className="h-screen relative -z-0 overflow-hidden gap-1 bg-[#222222] flex flex-col items-center justify-center"
             id="acara"
           >
-            Acara
+            <div className="w-[50%] absolute -z-20 top-20 left-0">
+              <img src="./assets/wayang1.png" alt="wayang1" />
+            </div>
+            <div className="w-[50%] absolute -z-20 bottom-0 right-0 -scale-x-100">
+              <img src="./assets/wayang1.png" alt="wayang1" />
+            </div>
+            <div className="text-white text-center w-full items-start flex flex-col px-6">
+              <p className="text-3xl font-Cardo">Akad</p>
+              <p className="text-xl font-Ribeye-marrow">
+                Pukul 07:00 - 09:00 WIB
+              </p>
+            </div>
+            <div className="w-full px-14">
+              <img src="./assets/tangan.png" alt="tangan" />
+            </div>
+            <div className="text-white text-center w-full items-end flex flex-col px-6">
+              <p className="text-3xl font-Cardo">Resepsi</p>
+              <p className="text-xl font-Ribeye-marrow">
+                Pukul 09:00 - 12:00 WIB
+              </p>
+            </div>
+            <div className="w-full relative flex flex-col place-items-center">
+              <p className="text-white font-Courgette text-3xl mt-6 bg-[#222222]">
+                Sabtu
+              </p>
+              <p className="text-4xl text-white font-Ribeye-marrow mt-2">
+                12.12.2024
+              </p>
+              <div className="absolute top-7 w-[80%] -z-10">
+                <img src="./assets/buletan.png" alt="buletan" />
+              </div>
+            </div>
+            <div className="mt-10 mb-5 text-center text-white">
+              <p className="text-xl font-Cardo font-medium ">
+                Kediaman mempelai wanita
+              </p>
+              <p className="font-Cardo">
+                Jl. Pamulang Raya 004/004 <br />
+                Kec.Pamulang Kab.Palang Merah
+              </p>
+            </div>
+            <div className="w-full px-10 pb-10 relative -z-0 place-items-center flex flex-col">
+              <img src="./assets/maps.png" alt="maps" />
+              <button className="px-5 py-1 rounded-2xl absolute gap-2 flex items-center bottom-14 hover:bg-amber-900 text-white text-sm z-10 bg-[#222222]">
+                <TbMapSearch size={20} className="text-white" />
+                Lihat dimaps
+              </button>
+            </div>
           </section>
+
           <section
-            className="h-screen bg-[#F5F0F4] flex flex-col items-center justify-evenly"
+            className="h-screen relative gap-4 -z-0 overflow-hidden bg-[#222222] flex flex-col items-center justify-center"
             id="galeri"
           >
-            Galeri
+            <div className="absolute top-20 left-0 -z-20">
+              <img src="./assets/wayanghiasan.png" alt="hiasanwayang" />
+            </div>
+            <div className="absolute bottom-0 right-0 -z-20 -scale-x-100">
+              <img src="./assets/wayanghiasan.png" alt="hiasanwayang" />
+            </div>
+            <div className="ml-28">
+              <img src="./assets/cincin.png" alt="cincin" />
+            </div>
+            <p className="text-4xl text-white font-Cardo px-28 text-center">
+              Kisah Kita Berdua
+            </p>
+            <div className="w-full flex justify-end gap-5 px-6">
+              <div className="w-[40%]">
+                <img src="./assets/foto1.png" alt="foto1" />
+              </div>
+              <div className="w-[40%]">
+                <img src="./assets/foto2.png" alt="foto2" />
+              </div>
+            </div>
+            <div className="w-full flex justify-start gap-5 px-6">
+              <div className="w-[40%]">
+                <img src="./assets/foto3.png" alt="foto3" />
+              </div>
+              <div className="w-[40%]">
+                <img src="./assets/foto4.png" alt="foto4" />
+              </div>
+            </div>
           </section>
+
           <section
-            className="h-screen bg-[#F5F0F4] flex flex-col items-center justify-evenly"
+            className="h-screen relative -z-0 overflow-hidden bg-[#222222] flex flex-col items-center justify-center"
             id="ucapan"
           >
-            Ucapan
+            <div className="absolute bottom-0 w-full -z-30 blur-sm opacity-75">
+              <img src="./assets/bgevent.png" alt="event" />
+            </div>
+            <div className="text-center text-white">
+              <p className="text-xl font-Parisienne">
+                Terhitung mundur dari sekarang
+              </p>
+              <p className="text-3xl font-Courgette">Menuju Hari-H</p>
+            </div>
+            <div className="text-white">
+              <Countdown
+                targetMonth={14}
+                targetDate={1}
+                targetHour={2}
+                targetMinute={10}
+              />
+            </div>
+            <div className="text-white text-3xl font-Courgette text-center mt-10 mb-5">
+              <p>
+                Ucapan Ke <br />
+                Calon Pengantin
+              </p>
+            </div>
+            <div className="w-full px-10 flex flex-col gap-5 items-end mb-20">
+              <input
+                placeholder="Nama Pengirim"
+                className="rounded-2xl px-5 py-2 bg-white w-full"
+              />
+              <textarea
+                placeholder="Kalimat ucapan untuk mempelai"
+                className="rounded-2xl bg-white px-5 py-2 w-full h-36"
+              />
+              <button className="px-5 py-2 rounded-2xl bg-amber-800 text-white hover:text-gray-500 hover:bg-amber-950 duration-200">
+                KIRIM
+              </button>
+            </div>
+          </section>
+
+          <section
+            id="gift"
+            className="h-screen relative -z-0 overflow-hidden gap-5 bg-[#222222] flex flex-col items-center justify-center"
+          >
+            <p className="text-center text-white font-Courgette text-4xl">
+              WEDDING GIFT
+            </p>
+            <div className="w-full px-10">
+              <div className="bg-white rounded-xl w-full flex justify-between px-3 py-3">
+                <div className="w-[40%]">
+                  <img src="./assets/bri.png" alt="bankbri" />
+                </div>
+                <div className="w-[60%] flex flex-col items-end">
+                  <p className="text-xl font-Cardo">Ahmad Almayda</p>
+                  <div className="h-[2px] w-[90%] bg-[#222222]"></div>
+                  <button className="flex items-center gap-1 mt-1">
+                    <LuCopy size={20} />
+                    <p className="text-sm">0021-01-005650-2</p>
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="w-full px-10">
+              <div className="bg-white rounded-xl w-full flex justify-between px-3 py-3">
+                <div className="w-[40%]">
+                  <img src="./assets/bca.png" alt="bankbca" />
+                </div>
+                <div className="w-[60%] flex flex-col items-end">
+                  <p className="text-xl font-Cardo">Mutiara Devania</p>
+                  <div className="h-[2px] w-[90%] bg-[#222222]"></div>
+                  <button className="flex items-center gap-1 mt-1">
+                    <LuCopy size={20} />
+                    <p className="text-sm">0021-01-005650-2</p>
+                  </button>
+                </div>
+              </div>
+            </div>
           </section>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     fontFamily: {
+      "DM-Serif-display": ['"DM Serif Display"', "serif"],
+      "Ribeye-marrow": ['"Ribeye Marrow"', "cursive"],
       Cardo: ['"Cardo"', "serif"],
+      Corinthia: ['"Corinthia"', "cursive"],
+      Parisienne: ['"Parisienne"', "cursive"],
+      Courgette: ['"Courgette"', "cursive"],
     },
     extend: {},
   },
